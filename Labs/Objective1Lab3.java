290054cc7c861d7bad7fe27d4cdcc7ce6f5e54bd
MJQuesada <MJQuesada45@gmail.com>
2019-02-27 12:04:17 -0500

    Exercise 1-6

---
+++ Labs/Objective1Lab3.java
@@ -0,0 +1,8 @@
+public class Objective1Lab3 {
+  public static void main (String [] args) {
+
+    System.out.println("Name: Michael Quesada");
+    System.out.println("Age: 27");
+    System.out.println("State: Florida");
+  }
+}
